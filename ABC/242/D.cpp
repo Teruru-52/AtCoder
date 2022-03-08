bb3e65037d99bbe02aff53d8ed8b50b7b26a7203
Teruru-52 <83484632+Teruru-52@users.noreply.github.com>
2022-03-08 14:07:32 +0900

    AC ABC242 D

---
+++ ABC/242/D.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    string S;
+    cin >> S;
+    int Q;
+    cin >> Q;
+
+    auto g = [&](char s, ll add)
+    {
+        return char('A' + (s - 'A' + add) % 3);
+    };
+
+    std::function<char(ll, ll)> f = [&](ll t, ll k)
+    {
+        if (t == 0)
+            return S[k];
+        if (k == 0)
+            return g(S[0], t);
+        return g(f(t - 1, k / 2), k % 2 + 1);
+    };
+
+    for(int i = 0; i < Q; i++)
+    {
+        ll t, k;
+        cin >> t >> k;
+        cout << f(t, k - 1) << endl;
+    }
+}
--- ABC/242/README.md
+++ ABC/242/README.md
@@ -1,7 +1,9 @@
 # [ABC 242](https://atcoder.jp/contests/abc242)
-### A
+### A-T-shirt
 出力桁数
-### B
+### B-Minimize Ordering
 文字列をSortで辞書順最小に並び替え
-### C
+### C-1111gal password
 2次元動的計画法
+### D-ABC Trandform
+周期性から再帰的に求解
